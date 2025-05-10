8dfa43620cfb777511f894975c753036177e8dbe
sharath <sharathkota070@gmail.com>
2025-05-11 02:38:57 +0530

    config nvim for nix

--- .conf/nvim/.config/nvim/lua/config/plugins/none-ls.lua
+++ .conf/nvim/.config/nvim/lua/config/plugins/none-ls.lua
@@ -1,12 +1,12 @@
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.goimports,
-                null_ls.builtins.formatting.stylua,
-            },
-        })
-    end,
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.goimports,
+        null_ls.builtins.formatting.stylua,
+      },
+    })
+  end,
 }
