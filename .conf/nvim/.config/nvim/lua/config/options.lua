49fcb9ec565b52d71b8e9e74128cf593f0fb3e5f
sharath <sharathkota070@gmail.com>
2025-05-11 00:38:57 +0530

    config nvim for nix

--- .conf/nvim/.config/nvim/lua/config/options.lua
+++ .conf/nvim/.config/nvim/lua/config/options.lua
@@ -10,6 +10,7 @@ set.number = true
 set.relativenumber = true
 set.tabstop = 4
 set.shiftwidth = 4
+set.softtabstop = 4
 set.expandtab = true
 set.virtualedit = "block"
 set.termguicolors = true
@@ -23,7 +24,7 @@ set.showmode = false
 -- schedules the clipboard setting to happen later — after neovim finishes starting up
 -- the function inside vim.schedule() runs after neovim has finished loading all plugins, ui events, etc.
 vim.schedule(function()
-	set.clipboard = "unnamedplus"
+  set.clipboard = "unnamedplus"
 end)
 
 -- enable break indent
@@ -63,9 +64,9 @@ set.scrolloff = 10
 set.confirm = true
 
 vim.api.nvim_create_autocmd("textyankpost", {
-	desc = "highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
